6431fbcc45c7fb5a0792da4618872aec966cf890
guadafestinese <guadalupefestinese@gmail.com>
2025-02-27 15:49:47 -0300

    .

--- widi/src/components/layout/Header2.jsx
+++ widi/src/components/layout/Header2.jsx
@@ -1,9 +1,18 @@
 import styled from 'styled-components'
 
 const Header2Div = styled.div`
-background-color:black;
-height: 50px;
-width:100%
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 15px;
+    height: 60px;
+    width: 100%;
+    background-color: rgba(0,0,0,0.9);
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 101;
+    box-shadow: 0px 3px 20px rgba(0,0,0,0.1);
 `
 const ImgHeader2 = styled.img`
 width:50px;
