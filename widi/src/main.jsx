c753e1fbdb41692ad3b3abf68c3396810177cd7e
guadafestinese <guadalupefestinese@gmail.com>
2025-02-26 21:10:27 -0300

    .

--- widi/src/main.jsx
+++ widi/src/main.jsx
@@ -3,19 +3,22 @@ import { createRoot } from "react-dom/client";
 import "./App.css";
 import App from "./App.jsx";
 //StateProvider initialState={initialState} reducer={reducer}
-import { Auth0Provider } from "@auth0/auth0-react";
+//import { Auth0Provider } from "@auth0/auth0-react";
 
 
 createRoot(document.getElementById("root")).render(
-  <Auth0Provider
-    domain="dev-7cmkqcbih4h2hfid.us.auth0.com"
-    clientId="ydFRqL0ECs16TSeLwFmmScPSrVC97q3S"
-    authorizationParams={{ redirect_uri: window.location.origin }}
-  >
+  
     <StrictMode>
       <div className="app">
         <App />
       </div>
     </StrictMode>
-  </Auth0Provider>
+  
 );
+
+
+/* <Auth0Provider
+    domain="dev-7cmkqcbih4h2hfid.us.auth0.com"
+    clientId="ydFRqL0ECs16TSeLwFmmScPSrVC97q3S"
+    authorizationParams={{ redirect_uri: window.location.origin }}
+  ></Auth0Provider> */
