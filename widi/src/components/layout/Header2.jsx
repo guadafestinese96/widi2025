3bf7966490cbb630710e718be30f5cc35484b8e7
guadafestinese <guadalupefestinese@gmail.com>
2025-02-27 15:44:39 -0300

    .

---
+++ widi/src/components/layout/Header2.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Header2Div = styled.div`
+background-color:black;
+height: 50px;
+`
+export default function Header2(){
+    return(
+        <Header2Div className="header">
+            dfsdfdslfdslf
+        </Header2Div>
+    )
+}
--- widi/src/components/layout/Layout.jsx
+++ widi/src/components/layout/Layout.jsx
@@ -1,11 +1,12 @@
 import Footer from "../footer/Footer";
 import Header from "../header/Header";
+import Header2 from "./Header2";
 import "./Layout.css";
 
 export default function Layout({ children }) {
   return (
     <div className="bodyLayout">
-   
+   <Header2/>
      <Footer/>
       
       <div className="childrenContainer">{children}</div>
