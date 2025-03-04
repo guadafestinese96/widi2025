95a99e7e8f6b832052e37183a959ed8113713f3d
guadafestinese <guadalupefestinese@gmail.com>
2025-03-04 18:45:30 -0300

    .

--- widi/src/components/redes/RedesContainer.jsx
+++ widi/src/components/redes/RedesContainer.jsx
@@ -1,72 +1,88 @@
-import { NavLink } from 'react-router-dom'
-import { Instagram, WhatsApp } from '@mui/icons-material'
-import styled from 'styled-components'
+import { NavLink } from "react-router-dom";
+import { Instagram, WhatsApp } from "@mui/icons-material";
+import styled from "styled-components";
+import { useContext } from "react";
+import CartContext from "../context/CartContext";
 
 const RedesContainerDiv = styled.div`
-display: flex;
-    justify-content: space-between;
-    width: 100vw;
-    align-items: center;
-    padding: 8px 25px 8px 25px;
-    bottom: 0;
-    position: fixed;
-    background-color: var(--colorSecundario);
-    border-radius: 25px 25px 0 0 ;
-`
+  display: flex;
+  justify-content: space-between;
+  width: 100vw;
+  align-items: center;
+  padding: 8px 25px 8px 25px;
+  bottom: 0;
+  position: fixed;
+  background-color: var(--colorSecundario);
+  border-radius: 25px 25px 0 0;
+`;
 const IgWpContainer = styled.div`
-display: flex;
-    justify-content: center;
-    align-items: center;
-    color: black;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: black;
+`;
 const IgWp = styled.div`
-background-color: var(--colorPrimario);
-    border-radius: 5px;
-    padding: 5px;
-    width: 28px;
-    height: 28px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-right: 5px;
-    color: black;
+  background-color: var(--colorPrimario);
+  border-radius: 5px;
+  padding: 5px;
+  width: 28px;
+  height: 28px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 5px;
+  color: black;
 
-    &:hover{
+  &:hover {
     color: var(--colorSecundario);
-    }
-`
+  }
+`;
 const IgWpP = styled.p`
-font-size: 12px;
-    font-weight: bold;
+  font-size: 12px;
+  font-weight: bold;
 
-    &:hover{
+  &:hover {
     color: var(--colorPrimario);
     transition: 0.5s;
-    }
-`
-export default function RedesContainer(){
-    const mensaje = encodeURIComponent("hola");
-    const phoneNumber = "5491161950112";
-    return(
-        <RedesContainerDiv>
-        <NavLink to="https://www.instagram.com/widistoreok__" target="_blank">
-          <IgWpContainer>
-            <IgWp>
-              <Instagram />
-            </IgWp>
-            <IgWpP>@widistoreok__</IgWpP>
-          </IgWpContainer>
-        </NavLink>
+  }
+`;
+export default function RedesContainer() {
+  const phoneNumber = "5491161950112";
+  const { cart } = useContext(CartContext);
+  console.log(cart);
+  
+  const itemsWpCart = cart.map((item) => item.marca + " " + item.nombre).join(", ");
+  
+  console.log(itemsWpCart);
 
-        <NavLink to={`https://wa.me/${phoneNumber}?text=${mensaje}`} target="_blank" rel="noopener noreferrer">
-          <IgWpContainer>
-            <IgWp>
-              <WhatsApp />
-            </IgWp>
-            <IgWpP>+549 1161950112</IgWpP>
-          </IgWpContainer>
-        </NavLink>
-      </RedesContainerDiv>
-    )
+  const mensaje = encodeURIComponent(
+    `Hola! Quiero comprar los perfumes ${itemsWpCart}`
+  );
+  console.log(mensaje);
+
+  return (
+    <RedesContainerDiv>
+      <NavLink to="https://www.instagram.com/widistoreok__" target="_blank">
+        <IgWpContainer>
+          <IgWp>
+            <Instagram />
+          </IgWp>
+          <IgWpP>@widistoreok__</IgWpP>
+        </IgWpContainer>
+      </NavLink>
+
+      <NavLink
+        to={`https://wa.me/${phoneNumber}?text=${mensaje}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <IgWpContainer>
+          <IgWp>
+            <WhatsApp />
+          </IgWp>
+          <IgWpP>+549 1161950112</IgWpP>
+        </IgWpContainer>
+      </NavLink>
+    </RedesContainerDiv>
+  );
 }
-
