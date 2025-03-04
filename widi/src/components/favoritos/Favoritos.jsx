67552a238f9bb030f2d9062139be03391bfcee42
guadafestinese <guadalupefestinese@gmail.com>
2025-03-04 16:01:50 -0300

    .

--- widi/src/components/favoritos/Favoritos.jsx
+++ widi/src/components/favoritos/Favoritos.jsx
@@ -1,15 +1,23 @@
 import { useContext } from "react"
 import CartContext from "../context/CartContext"
 import UnFavorito from "./UnFavorito";
+import styled from "styled-components";
 
+const FavContainer = styled.div`
+display:flex;
+justify-content:center;
+align-items:center;
+width:100vw;
+flex-wrap:wrap;
+`
 export default function Favoritos(){
     const {favoritos} = useContext(CartContext);
 
     return(
-        <div className="favContainer">
+        <FavContainer>
             {favoritos.length>0 ? favoritos.map(fav=>(
                 <UnFavorito perfume={fav} key={fav.id}/>
             )): <div>No hay favoritos</div>}
-        </div>
+        </FavContainer>
     )
 }
