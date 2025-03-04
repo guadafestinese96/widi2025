62b014e6d09e3fe60f9710d4269328cf99b7d8ae
guadafestinese <guadalupefestinese@gmail.com>
2025-03-04 14:56:11 -0300

    .

--- widi/src/components/layout/Layout.jsx
+++ widi/src/components/layout/Layout.jsx
@@ -2,15 +2,31 @@ import Footer from "../footer/Footer";
 import RedesContainer from "../redes/RedesContainer";
 import Header2 from "./Header2";
 import "./Layout.css";
+import styled from "styled-components";
 
+const BodyLayout = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
+const ChildrenContainer = styled.div`
+    display: flex;
+    margin-top: 130px;
+    padding: 20px;
+    justify-content: center;
+    align-items: center;   
+    padding-bottom: 80px;
+
+`
 export default function Layout({ children }) {
   return (
-    <div className="bodyLayout">
+    <BodyLayout>
 
       <Footer/>
       
-      <div className="childrenContainer">{children}</div>
+      <ChildrenContainer>{children}</ChildrenContainer>
       
-    </div>
+    </BodyLayout>
   );
 }
