608650a9ffc0821f9e9b90744aae4d4e2615a589
guadafestinese <guadalupefestinese@gmail.com>
2025-03-04 15:40:40 -0300

    .

--- widi/src/components/perfumesCard/ListPerfumesCard.jsx
+++ widi/src/components/perfumesCard/ListPerfumesCard.jsx
@@ -1,13 +1,22 @@
 import PerfumesCard from "./PerfumesCard";
 import "./PerfumesCard.css";
 import { useState } from "react";
+import styled from "styled-components";
+
+const ListContainerCard = styled.div`
+display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`
 
 export default function ListPerfumesCard({ perfumes }) {
   return (
-    <div className="listContainerCard">
+    <ListContainerCard>
       {perfumes.map((perfume) => (
         <PerfumesCard perfume={perfume} key={perfume.id} />
       ))}
-    </div>
+    </ListContainerCard>
   );
 }
--- widi/src/components/perfumesCard/PerfumesCard.jsx
+++ widi/src/components/perfumesCard/PerfumesCard.jsx
@@ -6,35 +6,118 @@ import {
 import "./PerfumesCard.css";
 import { useContext, useState } from "react";
 import CartContext from "../context/CartContext";
+import styled from "styled-components";
+
+const CardContainer = styled.div`
+ display: flex;
+  background-color: var(--colorSecundario);
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100px;
+  text-align: center;
+  height: 150px;
+  margin: 8px;
+  border-radius: 15px;
+  padding: 10px;
+  box-shadow: 0px 0px 5px rgba(0,0,0,0.5);
+  text-transform: capitalize;
+`
+
+const CardImg = styled.img`
+ width: 60px;
+  height: 60px;
+  margin: 5px;
+`
+const CardP = styled.p`
+font-size: 10px;
+  margin-top: 4px;
+`
+
+const CardH3 = styled.h3`
+font-size: 9px;
+`
+const CardH4 = styled.h4`
+font-size: 8px;
+`
+const BtnFavoritos = styled.button`
+background-color: unset;
+  border: none;
+  cursor: pointer;
+
+  &:hover{
+  color: var(--colorPrimario);
+  }
+`
+const ButtonsPerfumesCard = styled.div`
+display: flex;
+  justify-content: space-around;
+  align-items: center;
+`
+
+const AddToCart = styled.button`
+background-color: var(--colorSecundario);
+  border: none;
+  cursor: pointer;
+`
+const AddedToCart = styled.button`
+background-color: var(--colorSecundario);
+  border: none;
+  cursor: pointer;
+`
+
+
+// .cardContainer .addToCart .MuiSvgIcon-root, 
+// .cardContainer .addedToCart .MuiSvgIcon-root{
+//   color: var(--colorPrimario);
+// }
+
+
+// .cardContainer .MuiSvgIcon-root {
+//   width: 20px;
+//   margin-top: 5px;
+// }
+
+
+
+
+
+// .btnFavoritos ~ .adedToFav{
+//   color: var(--colorPrimario);
+// }
+
+const AddedToFav = styled.button`
+color: var(--colorPrimario);
+`
+
+
+
 
 export default function PerfumesCard({ perfume }) {
   const { cart, addToCart, favoritos, addToFavoritos } = useContext(CartContext);
-
   const isItemInCart = cart.find((item) => item.id === perfume.id);
-
-  
   const isItemInFav = favoritos.find(item=> item.id === perfume.id)
 
   return (
-    <div className="cardContainer">
-      <img src={`/images/${perfume.img}`} alt="imgCard" />
-      <h3>{perfume.marca}</h3>
-      <h4>{perfume.nombre}</h4>
-      <p>${perfume.precio}</p>
-      <div className="buttonsPerfumesCard">
+    <CardContainer>
+      <CardImg src={`/images/${perfume.img}`} alt="imgCard" />
+      <CardH3>{perfume.marca}</CardH3>
+      <CardH4>{perfume.nombre}</CardH4>
+      <CardP>${perfume.precio}</CardP>
+      <ButtonsPerfumesCard>
         {isItemInCart ? (
-          <button className="addedToCart">
+          <AddedToCart className="addedToCart">
             <CheckRounded />
-          </button>
+          </AddedToCart>
         ) : (
-          <button className="addToCart" onClick={() => addToCart(perfume)}>
+          <AddToCart className="addToCart" onClick={() => addToCart(perfume)}>
             <AddShoppingCartRounded />
-          </button>
+          </AddToCart>
         )}
-        <button className="btnFavoritos" onClick={()=>addToFavoritos(perfume)}>
+        <BtnFavoritos className="btnFavoritos" onClick={()=>addToFavoritos(perfume)}>
           <FavoriteRounded className={isItemInFav ? "addedToFav": ""}/>
-        </button>
-      </div>
-    </div>
+        </BtnFavoritos>
+      </ButtonsPerfumesCard>
+    </CardContainer>
   );
 }
